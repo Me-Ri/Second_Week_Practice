414e8d17cf587c5449d1d736f1640dbcd8585388
SanserzG <sbtop00@mail.ru>
2024-05-03 18:19:52 +0500

    refactoring

--- back/crud/upRef.php
+++ back/crud/upRef.php
@@ -9,8 +9,9 @@ $volume = $_POST['volume'];
 $demQ = $_POST['demQ'];
 $comment = $_POST['comments'];
 $mark = $_POST['mark'];
+$badReason = $_POST['bad-reason'];
 
-mysqli_query($connect, "UPDATE `student_practice`  SET id_student = '$idStud', difficults = '$dif',volume = '$volume', qualities ='$demQ', comment = '$comment', mark = '$mark' WHERE id_student = '$idStud'");
+mysqli_query($connect, "UPDATE `student_practice`  SET id_student = '$idStud', difficults = '$dif',volume = '$volume', qualities ='$demQ', comment = '$comment', mark = '$mark', badReason ='$badReason' WHERE id_student = '$idStud'");
 
 mysqli_query($connect, "UPDATE `student_opop` SET state = 'Подтверждает ОПОП' WHERE id_student = '$idStud' AND id_opop = '$idDir'");
 
