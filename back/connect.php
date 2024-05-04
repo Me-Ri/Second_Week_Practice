a1705199c38e1f2a093218b44c05da09151f1998
Spectr5032L <101048182+Spectr5032L@users.noreply.github.com>
2024-05-04 11:06:48 +0500

    clean before final post

--- back/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connect = mysqli_connect("127.0.0.1", "root", "", "second_week");
-if(!$connect) {
-    die('Ошибка подключения к бд');
-}
-?>
--- back/sign_login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../helpers.php';
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    logout();
-} 
-redirect('/index.php');
-?>
